3f6ca05a298c515e53b27cfc5c162633b1c0e8e6
Daniel Diprose <danield@unleashedsoftware.com>
2015-05-22 17:16:01 +1200

    Magic Ball added

--- net5WebPrototype1/src/net5WebPrototype1/Controllers/HomeController.cs
+++ net5WebPrototype1/src/net5WebPrototype1/Controllers/HomeController.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using ClassLibrary1;
+using System.Threading;
 
 namespace net5WebPrototype.Controllers
 {
     public class HomeController : Controller
     {
+        private static Random _rnd = new Random();
+
+        private static List<string> _db = new List<string> { "Yes", "No", "Definitely, yes", "I don't know", "Looks like, yes" };
+
         private IvNextFeature _nextFeature;
         public HomeController(IvNextFeature nextFeature)
         {
@@ -26,6 +31,14 @@ namespace net5WebPrototype.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult GetAnswer()
+        {
+            int index = _rnd.Next(_db.Count);
+            var answer = _db[index];
+            return Json(answer);
         }
 
         public IEnumerable<string> GetAll()
---
+++ net5WebPrototype1/src/net5WebPrototype1/Models/EightBallModel.cs
@@ -0,0 +1,8 @@
+﻿
+namespace net5WebPrototype1.Models
+{
+    public class EightBallModel
+    {
+        public string Question { get; set; }
+    }
+}
