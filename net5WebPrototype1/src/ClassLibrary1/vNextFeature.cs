8b7d1d7668709be76d108977739a3fc5c0e235bb
Daniel Diprose <danield@unleashedsoftware.com>
2015-05-22 14:17:35 +1200

    inital commit

---
+++ net5WebPrototype1/src/ClassLibrary1/vNextFeature.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace ClassLibrary1
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class vNextFeature : IvNextFeature
+    {
+        public vNextFeature()
+        {
+        }
+
+        public IEnumerable<string> GetList()
+        {
+            return new string[]{
+                "Cloud and server-optimized",
+                "ASP.NET MVC and Web API have been unified into a single programming model",
+                "No-compile developer experience",
+                "Dependency injection out of the box",
+                "Side by side - deploy the runtime and framework with your application",
+                "NuGet everything - even the runtime itself",
+                "All Open Source via the .NET Foundation and takes contributions"
+                };
+        }
+    }
+
+
+    public interface IvNextFeature
+    {
+        IEnumerable<string> GetList();
+    }
+}
---
+++ net5WebPrototype1/src/net5WebPrototype1/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using ClassLibrary1;
+
+namespace net5WebPrototype.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IvNextFeature _nextFeature;
+        public HomeController(IvNextFeature nextFeature)
+        {
+            this._nextFeature = nextFeature;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public IEnumerable<string> GetAll()
+        {
+            return this._nextFeature.GetList();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
